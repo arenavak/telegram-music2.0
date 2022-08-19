cd1e43fd452afb1462e4fb6448b82a3d4f067837
arenavak <96662378+arenavak@users.noreply.github.com>
2022-08-19 16:54:19 +0430

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_single_song(bot, update):
     logging.log(logging.INFO, f'start downloading')
     bot.send_message(chat_id=chat_id, text="Fetching...")
     if config["SPOTDL_DOWNLOADER"]:
-        os.system(f'spotdl {url} --st 10')
+        os.system(f'spotdl {url}')
     elif config["SPOTIFYDL_DOWNLOADER"]:
         os.system(f'spotifydl {url}')
     else:
